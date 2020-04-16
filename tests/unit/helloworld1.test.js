a36ee923092342b0e7fb5d054cc4a1699d85b84e
Matías Barrios <matias.barriosn@gmail.com>
2020-04-16 17:34:53 -0400

    feat: hello-world1 unit testing

---
+++ tests/unit/helloworld1.test.js
@@ -0,0 +1,14 @@
+const appRoot = require('app-root-path');
+const path = require('path');
+const debug = require('debug')(process.env.DEBUG_NAMESPACE + path.basename(__filename));
+
+const helloWorld1 = require(`${appRoot}/handlers/hello-world1`).lambdaHandler;
+
+describe('helloworld1 unit testing', () => {
+  test('testing the response value', async () => {
+    const response = await helloWorld1();
+    const body = JSON.parse(response.body);
+    debug(body);
+    expect(body.message).toBe('hello world1');
+  });
+});

f9d60292a625943011deb6d2ee0d7d5621e3e8ed
Matías Barrios <matias.barriosn@gmail.com>
2020-04-16 17:34:25 -0400

    feat: jest installed and configured

---
+++ tests/config/dev.config.js
@@ -0,0 +1,12 @@
+// const fs = require('fs');
+// const path = require('path');
+
+process.env.DEBUG = 'Masisa::test*';
+process.env.DEBUG_NAMESPACE = 'Masisa::test::';
+// const env = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../../env.json')));
+// process.env = {
+//   ...process.env,
+//   ...env.massiveByStoreFunction.CONFIG.SOAP,
+//   ...env.massiveByStoreFunction.CONFIG.PIM,
+//   ...env.putItemFunction,
+// };

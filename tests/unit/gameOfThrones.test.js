cb48db1f10f6fecb7db1378864b2b74c538aa866
Matías Barrios <matias.barriosn@gmail.com>
2020-04-20 13:43:04 -0400

    feat: printing dayjs to test cache

--- tests/unit/gameOfThrones.test.js
+++ tests/unit/gameOfThrones.test.js
@@ -1,6 +1,7 @@
 const appRoot = require('app-root-path');
 const path = require('path');
 const debug = require('debug')(process.env.DEBUG_NAMESPACE + path.basename(__filename));
+const day = require('dayjs');
 
 const lambda = require(`${appRoot}/handlers/game-of-thrones`).lambdaHandler;
 
@@ -8,6 +9,7 @@ describe('gameOfThrones unit testing', () => {
   test('testing the response value', async () => {
     const response = await lambda();
     const body = JSON.parse(response.body);
+    console.log(day());
     debug(body);
     expect(body.message).toBe('Jon Snow');
   });

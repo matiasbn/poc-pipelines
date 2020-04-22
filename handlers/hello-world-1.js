3e100ff5d10d5256a45b0f50311b7afe4eb459f5
Matías Barrios <mbarrios@atactech.cl>
2020-04-22 15:43:25 -0400

    mb original

--- handlers/hello-world-1.js
+++ handlers/hello-world-1.js
@@ -20,7 +20,7 @@ exports.lambdaHandler = async (event, context) => {
     response = {
       statusCode: 200,
       body: JSON.stringify({
-        message: 'hello world1-staging-3-asdasdsad',
+        message: 'hello world1-staging-3-asdasdasdassadas',
         // location: ret.data.trim()
       }),
     };

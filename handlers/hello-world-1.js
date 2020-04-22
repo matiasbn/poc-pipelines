9ab220686f430118073efb156bd33596fffabe09
Not Matías <bm@tactech.cl>
2020-04-22 14:46:00 -0400

    testing matiasbarriosn

--- handlers/hello-world-1.js
+++ handlers/hello-world-1.js
@@ -20,7 +20,7 @@ exports.lambdaHandler = async (event, context) => {
     response = {
       statusCode: 200,
       body: JSON.stringify({
-        message: 'hello world1-staging-3-cambiosasdasd',
+        message: 'hello world1-staging-3-cosas',
         // location: ret.data.trim()
       }),
     };

9f0699f034b18e978abdbdb8fa8f6e9f95854465
Matías Barrios <bm@tactech.cl>
2020-04-23 16:33:14 -0400

    testing branch permissions

--- handlers/hello-world-1.js
+++ handlers/hello-world-1.js
@@ -20,7 +20,7 @@ exports.lambdaHandler = async (event, context) => {
     response = {
       statusCode: 200,
       body: JSON.stringify({
-        message: 'hello world1-staging-3-some-testing-admin',
+        message: 'hello world1-staging-3-some-testing-bm-again',
         // location: ret.data.trim()
       }),
     };

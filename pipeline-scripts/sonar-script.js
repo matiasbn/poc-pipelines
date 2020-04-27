771e62264eae716e9877b56d40dce0c961c1263e
Matías Barrios <mbarrios@tactech.cl>
2020-04-27 11:44:38 -0400

    feat: real script

--- pipeline-scripts/sonar-script.js
+++ pipeline-scripts/sonar-script.js
@@ -4,8 +4,7 @@ const sonarResponse = async () => {
   try {
     const { data: { projectStatus: { conditions } } } = await axios.get('http://54.174.170.210:9000/api/qualitygates/project_status?projectKey=sonar');
     const accepted = conditions.every((condition) => condition.status === 'OK');
-    console.log(accepted);
-    process.exit(1);
+    process.exit(accepted ? 0 : 1);
   } catch (error) {
     process.exit(1);
   }

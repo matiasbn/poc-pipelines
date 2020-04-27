eb5c34c8f85a77a94bd8e0ba6f3e70f79f12e6bc
Matías Barrios <mbarrios@tactech.cl>
2020-04-27 12:11:19 -0400

    fix: checking projectStatus.status

--- pipeline-scripts/sonar-script.js
+++ pipeline-scripts/sonar-script.js
@@ -1,10 +1,10 @@
 const axios = require('axios');
 
 const sonarResponse = async () => {
+  const { SONAR_HOST } = process.env;
   try {
-    const { data: { projectStatus: { conditions } } } = await axios.get('http://54.174.170.210:9000/api/qualitygates/project_status?projectKey=sonar');
-    const accepted = conditions.every((condition) => condition.status === 'OK');
-    process.exit(accepted ? 0 : 1);
+    const { data: { projectStatus: { status } } } = await axios.get(`${SONAR_HOST}/api/qualitygates/project_status?projectKey=sonar`);
+    process.exit(status === 'OK' ? 0 : 1);
   } catch (error) {
     process.exit(1);
   }
